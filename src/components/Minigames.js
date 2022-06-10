2ffa54bd61d2e23c4395faf16c64fef47de13c91
Marcel-BK <MarcelBK@icloud.com>
2022-06-10 15:38:24 +0200

    add Landingpage

--- src/components/Minigames.js
+++ src/components/Minigames.js
@@ -1,27 +1,71 @@
 import React from 'react'
-import { Spinner, Card, Row, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Card, Row } from "react-bootstrap";
 
 const Minigames = () => {
   return (
     <>
-    <h1>Hello</h1>
-    <Card className="p-0">
-        <Card.Header className="p-0">
-          <div>
-            <h2 className="pl-id my-1">Memory</h2>
-          </div>
-        </Card.Header>
+    <div className='container-fluid'>
+        
+        <Row className='lp-head py-3 mb-3'>
+            <Link className='home-button mx-3' to={`/`}><i className='bi bi-house-door-fill'></i></Link>
+            <h1 className='lp-header mx-auto'>CHOOSE A GAME</h1>
+        </Row>
 
-        <Card.Body className="p-0">
-          <div className="d-flex align-items-center">
-            <img className='mx-auto my-1 pl-img' src='https://media.bisafans.de/7810ef5//pokemon/artwork/001.png' alt='Picture' />
-          </div>
-        </Card.Body>
+        <Row>
 
-        <Card.Footer className="p-0">
-          <h3 className="pl-name">Hi</h3>
-        </Card.Footer>
-    </Card>
+            <Link className="p-0 lp-card my-5 mx-auto" to={`/memory`}>
+                <Card className="p-0">
+                    <Card.Header className="p-0">
+                    <div>
+                        <h2 className="lp-title text-center py-2 my-1">Memory</h2>
+                    </div>
+                    </Card.Header>
+
+                    <Card.Body className="p-0">
+                    <div className="d-flex align-items-center">
+                        <img className='mx-auto mt-3 lp-img' src='https://w7.pngwing.com/pngs/286/777/png-transparent-brain-training-brain-games-brain-drain-mind-games-free-brain-games-shapes-memory-game-find-two-of-the-same-brain-text-people-logo-thumbnail.png' alt='Memory Logo' />
+                    </div>
+                    </Card.Body>
+                </Card>
+            </Link>
+
+
+            <Link className="p-0 lp-card my-5 mx-auto" to={`/tictactoe`}>
+                <Card className="p-0">
+                    <Card.Header className="p-0">
+                    <div>
+                        <h2 className="lp-title text-center py-2 my-1">TicTacToe</h2>
+                    </div>
+                    </Card.Header>
+
+                    <Card.Body className="p-0">
+                    <div className="d-flex align-items-center">
+                        <img className='mx-auto mt-3 lp-img' src='https://cdn-icons-png.flaticon.com/512/566/566294.png' alt='Memory Logo' />
+                    </div>
+                    </Card.Body>
+                </Card>
+            </Link>
+
+
+            <Link className="p-0 lp-card my-5 mx-auto" to={`/rockpaperscissors`}>
+                <Card className="p-0">
+                    <Card.Header className="p-0">
+                    <div>
+                        <h2 className="lp-title text-center py-2 my-1">Rock Paper Scissors</h2>
+                    </div>
+                    </Card.Header>
+
+                    <Card.Body className="p-0">
+                    <div className="d-flex align-items-center">
+                        <img className='mx-auto mt-3 lp-img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtqMcGe1UX89sVkoNP2Zc6zi1je0s_v12gtw&usqp=CAU' alt='Memory Logo' />
+                    </div>
+                    </Card.Body>
+                </Card>
+            </Link>
+
+        </Row>
+    </div>
     </>
   )
 }
